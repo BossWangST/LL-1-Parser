934c973bf9dbb2d6fd70182a3c0f820619fd46e6
BossWangST <bosswang1226@gmail.com>
2022-05-09 14:07:29 +0800

    Finished reading grammar rules

--- src/main/java/LL1_Parser.java
+++ src/main/java/LL1_Parser.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 class symbol {
     int type;//0 <-> Non-Terminals  1 <-> Terminals -1 <-> Empty
-    int feature;//0 <-> normal  1 <-> repeated  2 <-> optional
+    int feature;//0 <-> normal  1 <-> repeated  2 <-> optional  !3 <-> optional but repeated(only one)  !4 <-> repeated but optional(only one)
     String non_Terminal_name;
     int Terminal;//we only need to check if the token_id is correct
     int further_feature;//this is for tokens like RELOP etc.
@@ -51,33 +51,37 @@ public class LL1_Parser {
     HashMap<String, Integer> token_id;
     BufferedReader reader;
 
-    void insert_single_symbol(String r, ArrayList<symbol> current_body, int type) {
+    void insert_single_symbol(String r, ArrayList<symbol> current_body, int feature) {
         String[] pair = r.split(",");
         int identifier = Integer.valueOf(pair[0]);
         if (identifier == 0) {//it is a non-terminal
-            current_body.add(new symbol(identifier, type, pair[1]));
+            current_body.add(new symbol(identifier, feature, pair[1]));
         } else if (identifier == 1) {//it is a terminal
             if (pair.length > 2) {//token with index
-                current_body.add(new symbol(identifier, type, token_id.get(pair[1]), token_id.get(pair[2])));
+                current_body.add(new symbol(identifier, feature, token_id.get(pair[1]), token_id.get(pair[2])));
             } else {//token
-                current_body.add(new symbol(identifier, type, token_id.get(pair[1])));
+                current_body.add(new symbol(identifier, feature, token_id.get(pair[1])));
             }
+        } else {//empty
+            current_body.add(new symbol(identifier, -1));
         }
     }
 
-    void insert_symbol(String r, ArrayList<symbol> current_body, int type) {
-        switch (type) {
+    void insert_symbol(String r, ArrayList<symbol> current_body, int feature) {
+        switch (feature) {
             case 0://normal pair
-                insert_single_symbol(r, current_body, type);
+                insert_single_symbol(r, current_body, feature);
                 break;
             case 1://repeated
             case 2://optional
+            case 3://optional_but_repeated
+            case 4://repeated_but_optional
                 Pattern p = Pattern.compile("^\\{(.*?)\\}");
                 while (!r.equals("")) {
                     Matcher m = p.matcher(r);
                     if (m.find()) {
                         String current_single = m.group(1);
-                        insert_single_symbol(current_single, current_body, type);
+                        insert_single_symbol(current_single, current_body, feature);
                         r = r.substring(m.group(0).length(), r.length());
                     }
                 }
@@ -89,9 +93,9 @@ public class LL1_Parser {
 
     rule[] get_rules(String rules) {
         String[] all_rule = rules.split("\\$");
-        Pattern p_optional = Pattern.compile("^\\{\\[\s*(.*?)\s+\\]\\}");
-        Pattern p_repeated = Pattern.compile("^\\{{2}\s*(.*?)\s+\\}{2}");
-        Pattern p_normal = Pattern.compile("^\\{(.*?)\\}");
+        Pattern p_optional = Pattern.compile("^\\{\\[\s*(.*?)\s*\\]\\}");
+        Pattern p_repeated = Pattern.compile("^\\{{2}\s*(.*?)\s*\\}{2}");
+        Pattern p_normal = Pattern.compile("^\\{\s*(.*?)\s*\\}");
         String[] pair;
         rule[] res = new rule[all_rule.length];
         int index = 0;
@@ -105,12 +109,23 @@ public class LL1_Parser {
                 Matcher m_optional = p_optional.matcher(str);
                 if (m_repeated.find()) {
                     String current_symbol = m_repeated.group(1);
-                    insert_symbol(current_symbol, current_body, 1);
-                    //str = str.substring(0, current_symbol.length() + 4);
+                    Pattern p_repeated_optional = Pattern.compile("^\\{\\[\s*(.*?)\s*\\]\\}");
+                    Matcher m_repeated_optional = p_repeated_optional.matcher(current_symbol);
+                    if (m_repeated_optional.find()) {
+                        insert_symbol(m_repeated_optional.group(1), current_body, 4);
+                    } else {
+                        insert_symbol(current_symbol, current_body, 1);
+                    }
                     str = str.substring(m_repeated.group(0).length(), str.length());
                 } else if (m_optional.find()) {
                     String current_symbol = m_optional.group(1);
-                    insert_symbol(current_symbol, current_body, 2);
+                    Pattern p_optional_repeated = Pattern.compile("^\\{\\{\s*(.*?)\s*\\}\\}");
+                    Matcher m_optional_repeated = p_optional_repeated.matcher(current_symbol);
+                    if (m_optional_repeated.find()) {
+                        insert_symbol(m_optional_repeated.group(1), current_body, 3);
+                    } else {
+                        insert_symbol(current_symbol, current_body, 2);
+                    }
                     str = str.substring(m_optional.group(0).length(), str.length());
                 } else if (m_normal.find()) {
                     String current_symbol = m_normal.group(1);
@@ -132,8 +147,10 @@ public class LL1_Parser {
         this.rules = new HashMap<String, rule[]>();
         String current_line = "";
         int flag = 0;
-        while (current_line != null) {
+        while (true) {
             current_line = reader.readLine();
+            if (current_line == null)
+                break;
             if (current_line.equals("") || current_line.charAt(0) == '#') continue;
             if (current_line.contains("%%")) {
                 flag++;
@@ -143,9 +160,10 @@ public class LL1_Parser {
                 String[] token_name = current_line.split("\s+");
                 token_id.put(token_name[0], Integer.valueOf(token_name[1]));
             } else {
+                System.out.println(current_line);
                 String[] current_rule = current_line.split("\s+", 2);
                 rule[] current_rules = get_rules(current_rule[1]);
-                this.rules.put(current_rule[0], current_rules);
+                this.rules.put(current_rule[0].substring(1, current_rule[0].length() - 1), current_rules);
             }
         }
     }
--- src/main/java/regex_test.java
+++ src/main/java/regex_test.java
@@ -3,8 +3,8 @@ import java.util.regex.Pattern;
 
 public class regex_test {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("^\\{\\{(.*?)\\}\\}");
-        Matcher m = p.matcher("{{haha}}");
+        Pattern p = Pattern.compile("^\\{\\[\s*(.*?)\s*\\]\\}");
+        Matcher m = p.matcher("{[{1,PUNCTUATION,LSBR}{1,PUNCTUATION,RSBR}]}");
         if (m.find()) {
             System.out.println(m.group(1));
         } else System.out.println("NOT FOUND");
